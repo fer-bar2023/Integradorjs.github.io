8d99167721cf0cb2a8236398ddeb59aa405b7b95
fer-bar2023 <129174256+fer-bar2023@users.noreply.github.com>
2023-05-26 23:15:55 -0300

    initial commit

---
+++ js/tickets.js
@@ -0,0 +1,20 @@
+/*let precioTicket = 200;
+function descuento (precioTicket, porcentaje){
+   return precioTicket - (precioTicket * porcentaje / 100)
+};
+let ticketEstudiante = descuento (precioTicket, 80);
+let ticketTrainee = descuento (precioTicket, 50);
+let ticketJunior = descuento (precioTicket, 15);
+var selectElement = document.getElementById("categoria");
+var selectedValue = selectElement.value;
+
+function calcularPrecio () {
+   let cantidad = document.getElementById("cantidad").value;
+   
+   console.log(cantidad * ticketEstudiante);
+   console.log(selectedValue)
+};
+*/
+
+//hay que lograr : el precio con descuento  multiplicado por la cantidad de tickets a comprar 
+
